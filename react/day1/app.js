587b6babadf2252bf0bd6a1c94019cd7ee882107
buyath <buyaththayub@gmail.com>
2025-12-11 01:25:37 +0000

    i am working in react projects

--- react/day1/app.js
+++ react/day1/app.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Counter from "./components/Counter";
 import User from "./components/User";
-
+import Theme from "./components/Toggle";
 const App = () => {
     return (
-        <div>
-           <User/>
+        <div >
+           <Theme/>
         </div>
     )
-}
+} 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App/>);
---
+++ react/day1/components/Toggle.js
@@ -0,0 +1,9 @@
+const Theme = () => {
+    return (
+        <div >
+            <h1>Theme Switcher</h1>
+        </div>
+    )
+}
+
+export default Theme;
